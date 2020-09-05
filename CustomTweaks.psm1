11694e3a612e490609eb96bc92706a10d69be312
Daniel Berntsson <dberntsson.dev@gmail.com>
2020-09-05 19:06:07 +0200

    Initial commit

---
+++ CustomTweaks.psm1
@@ -0,0 +1,6 @@
+##########
+# Win 10 / Server 2016 / Server 2019 Initial Setup Script - Custom Tweak library
+# Author: Daniel Berntsson <dberntsson.dev@gmail.com>
+# Version: v0.1.0, 2020-09-05
+# Source: https://github.com/daniel-berntsson/Win10-Initial-Setup-Script
+##########
